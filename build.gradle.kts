0a4e620b2d6368f3553ffe6e27c4a390c3e46409
Harold Martin <harold@jump.com>
2023-08-07 08:19:58 -0700

    Version upgrades and doc workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.android.library") version libs.versions.android.gradle.plugin apply false
     id("org.jetbrains.kotlin.android") version libs.versions.kotlin apply false
     id("org.jetbrains.dokka") version libs.versions.dokka
-    id("org.sonarqube") version "3.5.0.2730"
+    id("org.sonarqube") version "4.3.0.3225"
     alias(libs.plugins.gradleVersions)
 }
 
@@ -12,11 +12,20 @@ sonarqube {
         property("sonar.projectKey", "jump-sdk_jetpack_compose_country_code_picker_emoji")
         property("sonar.organization", "jump-sdk")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.kotlin.source.version", "1.8.10")
+        property("sonar.kotlin.source.version", "1.9.0")
         property("sonar.pullrequest.github.summary_comment", "true")
     }
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
+}
+
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    checkForGradleUpdate = true
+    rejectVersionIf {
+        listOf("-mercury-", "-station-", "-alpha", "-dev-").any { word ->
+            candidate.version.contains(word)
+        }
+    }
 }
